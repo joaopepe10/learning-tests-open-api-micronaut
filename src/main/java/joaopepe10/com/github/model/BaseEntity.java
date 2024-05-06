78825e581171c0abccedfc1e3b31f1964b154c32
João Perdro Pires <joaopedropires61@gmail.com>
2024-05-05 21:37:57 -0300

    feat: create BaseEntity

---
+++ src/main/java/joaopepe10/com/github/model/BaseEntity.java
@@ -0,0 +1,22 @@
+package joaopepe10.com.github.model;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@MappedSuperclass
+public class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    protected UUID id;
+
+    @CreationTimestamp
+    protected LocalDateTime createdAt;
+}
