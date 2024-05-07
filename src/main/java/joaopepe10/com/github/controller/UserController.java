4c29fd89222c5c5a0554492ef7162c68cc4b1e5d
João Perdro Pires <joaopedropires61@gmail.com>
2024-05-06 22:39:13 -0300

    feat: Implement method save

--- src/main/java/joaopepe10/com/github/controller/UserController.java
+++ src/main/java/joaopepe10/com/github/controller/UserController.java
@@ -4,18 +4,19 @@ import io.micronaut.http.annotation.Controller;
 import joaopepe10.com.github.UserApi;
 import joaopepe10.com.github.model.UserRequest;
 import joaopepe10.com.github.model.UserResponse;
+import joaopepe10.com.github.service.UserService;
+import lombok.RequiredArgsConstructor;
 
-import java.time.ZonedDateTime;
-import java.util.UUID;
+import java.util.List;
 
 @Controller("/api/v1")
+@RequiredArgsConstructor
 public class UserController implements UserApi {
+    private final UserService userService;
+
     @Override
-    public UserResponse createUser(UserRequest userRequest) {
-        UserResponse response = new UserResponse();
-        response.setCreatedAt(ZonedDateTime.now());
-        response.setName("Joao Pedro");
-        response.setId(String.valueOf(UUID.randomUUID()));
+    public UserResponse save(UserRequest userRequest) {
+        var response = userService.save(userRequest);
         return response;
     }
 }
