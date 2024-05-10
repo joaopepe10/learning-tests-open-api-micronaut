373379ff427c91780b93b4c9b7f23275b575f2bf
João Perdro Pires <joaopedropires61@gmail.com>
2024-05-09 22:45:04 -0300

    feat: Insert pattern builder for User

--- src/main/java/joaopepe10/com/github/model/User.java
+++ src/main/java/joaopepe10/com/github/model/User.java
@@ -2,17 +2,24 @@ package joaopepe10.com.github.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
+import lombok.*;
+
+import java.time.ZonedDateTime;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
 @Entity
 @Table(name = "tbl_users")
-@NoArgsConstructor
 @AllArgsConstructor
 public class User extends BaseEntity{
     private String name;
+    @Builder
+    public User(Long id, ZonedDateTime createdAt, String name) {
+        super(id, createdAt);
+        this.name = name;
+    }
+
+    public User() {
+        super();
+    }
 }
